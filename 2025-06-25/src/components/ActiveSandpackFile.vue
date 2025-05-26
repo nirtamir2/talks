c889c94c5ad164b132deab58681ff52960d79c58
Nir Tamir <nirtamir2@gmail.com>
2025-05-27 00:20:23 +0300

    WIP - feat(sandpack): Add ActiveSandpackFile component

---
+++ 2025-06-25/src/components/ActiveSandpackFile.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useSandpack } from "sandpack-vue3";
+const { sandpack } = useSandpack()
+
+</script>
+<template>
+    <div >{{ sandpack.visibleFiles }}</div>
+    <pre>{{ sandpack.files[sandpack.activeFile].code }}</pre>
+</template>
