663f6c3f3908cbe9de24985311ad97c545e0cc3a
Nir Tamir <nirtamir2@gmail.com>
2025-12-09 10:59:44 -0500

    WIP - Remove RubiksCube component from demo route

--- 2025-06-25/demo/src/routes/demo/cube.tsx
+++ 2025-06-25/demo/src/routes/demo/cube.tsx
@@ -1,7 +1,6 @@
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { createFileRoute } from "@tanstack/react-router";
-import { RubiksCube } from "~/components/RubiksCube";
 
 export const Route = createFileRoute("/demo/cube")({
   component: RouteComponent,
@@ -13,7 +12,6 @@ function RouteComponent() {
       <PerspectiveCamera />
       <OrbitControls />
       <ambientLight />
-      <RubiksCube />
     </Canvas>
   );
 }
