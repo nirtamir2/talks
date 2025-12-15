88783aff9174ec3280386358d597d2be4f575999
Nir Tamir <nirtamir2@gmail.com>
2025-12-16 00:54:35 +0200

    Revert "WIP - chore: Update global-top.vue and slides.md for improved readability and consistency"
    This reverts commit 6b2bfc2c8a7e024d3ecbc798a1e36b85898b0ac0.

--- 2025-06-26/src/global-top.vue
+++ 2025-06-26/src/global-top.vue
@@ -10,11 +10,11 @@
 }
 
 code {
-  --prism-font-size: 18px;
+  --prism-font-size: 16px;
 }
 
 .slidev-code {
-  --slidev-code-font-size: 18px;
+  --slidev-code-font-size: 16px;
   --slidev-code-line-height: 22px;
 }
 </style>
