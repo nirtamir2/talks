b04e65c0c6d0d4ab37a68290742535a5233b5bbf
Nir Tamir <nirtamir2@gmail.com>
2026-07-12 12:10:23 +0300

    feat: define Sandpack addon public contracts

---
+++ packages/slidev-addon-sandpack/src/index.ts
@@ -0,0 +1,13 @@
+export { normalizeSandpackFileMap, normalizeSandpackPath } from "./paths.js";
+export {
+  defineSandpackConfig,
+  type ResolvedSandpackLayout,
+  type SandpackConfig,
+  type SandpackDemo,
+  type SandpackDemoFile,
+  type SandpackDemoStep,
+  type SandpackLayoutOptions,
+  type SandpackPredefinedTemplate,
+  type SandpackPreset,
+  type SandpackPresetFile,
+} from "./types.js";
